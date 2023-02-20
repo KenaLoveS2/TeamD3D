7a2932f8afc21764f612dfaec25d26fb4ab7e3c8
heyonii <hyewoooon2@naver.com>
2023-02-20 13:52:49 +0900

    Effect Add Resource

---
+++ Client/private/Imgui_Effect.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "..\public\Imgui_Effect.h"
+
+CImgui_Effect::CImgui_Effect(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+	: CImguiObject(pDevice, pContext)
+{
+}
+
+HRESULT CImgui_Effect::Initialize(void * pArg)
+{
+	m_szWindowName = "EffectTool";
+	return S_OK;
+}
+
+void CImgui_Effect::Imgui_RenderWindow()
+{
+
+}
+
+CImgui_Effect* CImgui_Effect::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext, void * pArg)
+{
+	CImgui_Effect * pInstance = new CImgui_Effect(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("CImgui_Effect Create Failed");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+void CImgui_Effect::Free()
+{
+}
