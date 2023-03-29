672fc18872f1f642483fb588dae09e3a1db73849
sigh5 <54612487+sigh5@users.noreply.github.com>
2023-03-29 13:13:29 +0900

    Merge Edit

--- Client/private/Level_MapTool.cpp
+++ Client/private/Level_MapTool.cpp
@@ -133,7 +133,7 @@ HRESULT CLevel_MapTool::Ready_Layer_BackGround(const _tchar * pLayerTag)
 	//CImgui_TerrainEditor::LoadFilterData("0_Terrain.json");
 	//CImgui_TerrainEditor::LoadFilterData("1_Terrain.json");
 	//CImgui_TerrainEditor::LoadFilterData("2_Terrain.json");
-	CImgui_TerrainEditor::LoadFilterData("3_Terrain.json");
+	//CImgui_TerrainEditor::LoadFilterData("3_Terrain.json");
 	CImgui_TerrainEditor::LoadFilterData("4_Terrain.json");
 
 	RELEASE_INSTANCE(CGameInstance);
