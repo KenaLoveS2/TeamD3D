3cdc0adc987bcbc0731f0a7a26a4aeb5e33ca5d8
sigh5 <54612487+sigh5@users.noreply.github.com>
2023-04-02 14:12:17 +0900

    .

--- Client/private/Loader.cpp
+++ Client/private/Loader.cpp
@@ -403,6 +403,7 @@ HRESULT CLoader::Loading_ForMapTool()
 	if (FAILED(pGameInstance->Add_Prototype(TEXT("Prototype_GameObject_Frog"), CFrog::Create(m_pDevice, m_pContext)))) return E_FAIL;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	PivotMatrix = XMMatrixScaling(0.005f, 0.005f, 0.005f);
 	if (FAILED(pGameInstance->Add_Prototype(LEVEL_MAPTOOL, L"Prototype_Component_Model_Dy_RockSmall03",
 		CModel::Create(m_pDevice, m_pContext, TEXT("../Bin/Resources/NonAnim/Rock/Rock_Small/Rock_Small_03.model"), 
@@ -432,6 +433,8 @@ HRESULT CLoader::Loading_ForMapTool()
 >>>>>>> parent of 285d1e32 (.)
 =======
 >>>>>>> parent of 285d1e32 (.)
+=======
+>>>>>>> parent of d2ba3816 (.)
 =======
 >>>>>>> parent of d2ba3816 (.)
 #pragma  endregion ANIM_OBJ
